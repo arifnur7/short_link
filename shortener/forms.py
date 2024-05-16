dfcb23dcc5cf26412841d2c48366de713f0bbc8e
Arif Nur Rohman <arifnur777@gmail.com>
2024-05-16 16:20:58 +0700

    Frist Step to create shorlink website
    1. Analytics for access link done
    2. Validasi dengan built in DJango dan javascript done
    3. logging error done
    next ---
    1. Login for history shortlink (in progress)
    2. Navbar and UI for form

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import ShortenedURL
+import logging
+
+logger = logging.getLogger(__name__)
 
 class URLForm(forms.ModelForm):
     class Meta:
@@ -10,11 +13,18 @@ class URLForm(forms.ModelForm):
                                         {'placeholder':'Optional : Costum Short URL'})
         }
 
+    forbidden_words = {'admin', 'root', 'support', 'help', 'login', 'register',}
+
     def clean_short_url(self):
         short_url = self.cleaned_data.get('short_url')
         if short_url:
+            #cek bila custom URL sudah digunakan
             if ShortenedURL.objects.filter(short_url=short_url).exists():
                 raise forms.ValidationError('This short URL is already taken.')
+            #cek bila costum URL berisi kata yang dilarang
+            if any(word in short_url.lower() for word in self.forbidden_words):
+                raise forms.ValidationError('Costume URL contain Forbidden Words or is reserved.')
+            #cek bila costum URL berisi selain alfanumeric
             if not short_url.isalnum():
                 raise  forms.ValidationError('Costume Short URL can only contain alphanumeric characters.')
         return short_url
--- shortener/views.py
+++ shortener/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import ShortenedURL
+from .models import ShortenedURL, URLAccess
 from .forms import URLForm
+import logging
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
+# Define the logger
+logger = logging.getLogger(__name__)
 
 def index(request):
     if request.method == 'POST':
@@ -18,12 +23,32 @@ def index(request):
     return render(request, 'index.html', {'form': form})
 
 def redirect_url(request, short_url):
-    url_instance = get_object_or_404(ShortenedURL, short_url=short_url)
-    # track url access
-    URLAccess.objects.create(shortened_url=url_instance)
-    return redirect(url_instance.original_url)
+    try:
+        url_instance = get_object_or_404(ShortenedURL, short_url=short_url)
+        # track url access
+        URLAccess.objects.create(shortened_url=url_instance)
+        return redirect(url_instance.original_url)
+    except Exception as e:
+        logger.error(f"Error in redirecting short URL {short_url}: {str(e)}")
+        return HttpResponse("An error occurred.", status=500)
 
 def analytics(request, short_url):
-    url_instance = get_object_or_404(ShortenedURL, short_url=short_url)
-    accesses = URLAccess.objects.filter(shortened_url=url_instance)
-    return render(request, 'analytics.html', {'url_instance': url_instance, 'accesses': accesses})
+    try:
+        url_instance = get_object_or_404(ShortenedURL, short_url=short_url)
+        accesses = URLAccess.objects.filter(shortened_url=url_instance)
+        return render(request, 'analytics.html', {'url_instance': url_instance, 'accesses': accesses})
+    except Exception as e:
+        logger.error(f"Error in accessing analytics for short URL {short_url}: {str(e)}")
+        return HttpResponse("An error occurred.", status=500)
+
+#menambahkan user login
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request,user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request,'register.html', {'form':form})
