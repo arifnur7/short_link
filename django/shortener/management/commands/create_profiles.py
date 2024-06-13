d99f5217ab1a941fcd6f51560076bf6d1435d5a6
Arif Nur Rohman <arifnur777@gmail.com>
2024-06-13 17:29:34 +0700

    Second Project create shorlink website:
    1. instalasi docker and nginx
    2. collectstatic files

---
+++ django/shortener/management/commands/create_profiles.py
@@ -0,0 +1,15 @@
+# shortener/management/commands/create_profiles.py
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.shortener.models import Profile
+
+class Command(BaseCommand):
+    help = 'Create missing profile objects for users'
+
+    def handle(self, *args, **kwargs):
+        users = User.objects.all()
+        for user in users:
+            if not hasattr(user, 'profile'):
+                Profile.objects.create(user=user)
+                self.stdout.write(self.style.SUCCESS(f'Created profile for user {user.username}'))
+        self.stdout.write(self.style.SUCCESS('All missing profiles have been created.'))
---
+++ django/shortener/management/commands/delete_expired_links.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.shortener.models import ShortenedURL
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = 'Delete expired short links that have been expired for more than 1 month'
+
+    def handle(self, *args, **kwargs):
+        one_month_ago = timezone.now() - timedelta(days=30)
+        expired_links = ShortenedURL.object.filter(status='expired', expired_at__1t = one_month_ago)
+        count = expired_links.count()
+        expired_links.delete()
+        self.stdout.write(self.style.SUCCESS(f'Delete {count} expired short link'))
